b997d4147d70e781793ab9b01519058f0af75187
Mohamed Nour <mohamed-nour.bechdli@enis.tn>
2025-10-09 12:43:27 +0100

    i wraped the approutes in the contextprovider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import AppRoutes from "./routes/AppRoutes";
+import ContextProvider from "./context/ContextProvider";
 
 function App() {
   return (
-    <>
+    <ContextProvider>
       <AppRoutes />
-    </>
+    </ContextProvider>
   );
 }
 
 export default App;
+
 
