e22d3b56f03c01462b24ed9d6f43dde3fdd35afd
Mohamed Nour <mohamed-nour.bechdli@enis.tn>
2025-10-09 08:59:01 +0100

    added a products array in productlist

--- client/src/components/products/ProductList.jsx
+++ client/src/components/products/ProductList.jsx
@@ -1,6 +1,11 @@
 import ProductCard from './ProductCard';
 
 const ProductList = () => {
+    const products = [
+        { id: 1, name: "Apple Watch", price: 599, image: "/images/apple-watch.png" },
+        { id: 2, name: "Gaming Laptop", price: 1299, image: "/images/laptop.png" },
+        { id: 3, name: "Smartphone", price: 999, image: "/images/phone.png" },
+    ];
     return (
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-10">
             <h2 className="text-2xl sm:text-3xl font-bold mb-6 text-gray-900 dark:text-white">
