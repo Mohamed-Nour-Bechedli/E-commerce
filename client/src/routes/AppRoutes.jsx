b92aaba3c2b3f0328a7818f64446aeb206f37a20
Mohamed Nour <mohamed-nour.bechdli@enis.tn>
2025-10-09 12:41:39 +0100

    I seperated the main from teh header and the footer

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,31 +1,37 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "../components/layout/Navbar";
 import Footer from "../components/layout/Footer";
+
 import Home from "../pages/Home";
-// import NotFound from "./pages/NotFound";
 import ProductDetails from "../pages/ProductDetails";
 import Cart from "../pages/Cart";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Checkout from "../pages/Checkout";
+// import NotFound from "../pages/NotFound";
 
 const AppRoutes = () => {
     return (
-        <div>
+        <div className="flex flex-col min-h-screen">
             <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                {/* <Route path="*" element={<NotFound />} /> */}
-                <Route path="/product/:id" element={<ProductDetails />} />
-                <Route path="/cart" element={<Cart />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/checkout" element={<Checkout />} />
-            </Routes>
-            <Footer />
-        </div >
-    )
-}
 
-export default AppRoutes
+            <main className="flex-1">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/product/:id" element={<ProductDetails />} />
+                    <Route path="/cart" element={<Cart />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/checkout" element={<Checkout />} />
+                    {/* <Route path="*" element={<NotFound />} /> */}
+                </Routes>
+            </main>
+
+            <Footer />
+        </div>
+    );
+};
+
+export default AppRoutes;
+
 
