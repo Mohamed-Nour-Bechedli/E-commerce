87385f1fefa84bcd6eb692f6c1b54c4ebb3ed02b
Mohamed Nour <mohamed-nour.bechdli@enis.tn>
2025-10-09 10:39:51 +0100

    Created the contextprovider

---
+++ client/src/context/ContextProvider.jsx
@@ -0,0 +1,15 @@
+import AuthContextProvider from "./AuthContext";
+import CartContextProvider from "./CartContext";
+import ProductContextProvider from "./ProductContext";
+
+const ContextProvider = ({ children }) => {
+    return (
+        <AuthContextProvider>
+            <ProductContextProvider>
+                <CartContextProvider>{children}</CartContextProvider>
+            </ProductContextProvider>
+        </AuthContextProvider>
+    );
+};
+
+export default ContextProvider;
