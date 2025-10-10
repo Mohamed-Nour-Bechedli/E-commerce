cc0cb8ebbc135c0d829ae7d47293e32aa9338f58
Mohamed Nour <mohamed-nour.bechdli@enis.tn>
2025-10-10 15:53:25 +0100

    Connected to database

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const db = require('./config/db');
+const app = express();
+
+
+
+
+// Connect to database
+db()
+
+port = 5000
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
