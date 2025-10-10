fee219fb87526e5a43a587b9321215d14f48216b
Mohamed Nour <mohamed-nour.bechdli@enis.tn>
2025-10-10 17:42:50 +0100

    added authrole middleware to protect route

---
+++ server/middlewares/role.js
@@ -0,0 +1,17 @@
+const User = require('../models/user');
+
+const role = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.user._id);
+        if (user.role !== 'admin') {
+            return res.status(403).json({ message : 'Access denied, admin only' });
+        }
+
+        next();
+
+    } catch (error) {
+        res.status(500).json({ message : "Server error", error : error.message });
+    }
+}
+
+module.exports = role;
