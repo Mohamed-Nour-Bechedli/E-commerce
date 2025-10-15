73f7260fd86f0ac306f021c0c7e83e06692f5ae4
Mohamed Nour <mohamed-nour.bechdli@enis.tn>
2025-10-15 11:53:48 +0100

    Created the axios instance

--- client/src/api/axiosConfig.js
+++ client/src/api/axiosConfig.js
@@ -1,1 +1,36 @@
 import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api", 
+  withCredentials: true, 
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Add a request interceptor 
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+// handle global responses or errors
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    // handle token expiration
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem("token");
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
