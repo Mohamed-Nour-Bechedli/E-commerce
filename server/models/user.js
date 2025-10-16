4f031f745e8ac89f0bc0d53679cb23755daad465
Mohamed Nour <mohamed-nour.bechdli@enis.tn>
2025-10-16 11:18:55 +0100

    added dedault avatar icon

--- server/models/user.js
+++ server/models/user.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
     name : { type : String, required : true },
     email : { type : String, required : true, unique : true },
     password : { type : String, required : true },
-    image : { type : String },
+    image : { type : String, default : "https://cdn-icons-png.flaticon.com/512/3135/3135715.png" },
     role : { type : String, enum : ['user', 'admin'], default : 'user' },
     verified : { type : Boolean, default : false },
 });
