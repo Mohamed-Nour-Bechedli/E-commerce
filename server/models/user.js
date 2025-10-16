c2545c342c39d9f40b0e53ffdbd16908a2702024
Mohamed Nour <mohamed-nour.bechdli@enis.tn>
2025-10-16 13:23:46 +0100

    added phone field

--- server/models/user.js
+++ server/models/user.js
@@ -2,23 +2,25 @@ const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
-    name : { type : String, required : true },
-    email : { type : String, required : true, unique : true },
-    password : { type : String, required : true },
-    image : { type : String, default : "https://cdn-icons-png.flaticon.com/512/3135/3135715.png" },
-    role : { type : String, enum : ['user', 'admin'], default : 'user' },
-    verified : { type : Boolean, default : false },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    image: { type: String, default: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png" },
+    role: { type: String, enum: ['user', 'admin'], default: 'user' },
+    verified: { type: Boolean, default: false },
+    phone: { type: String, default: "" } 
 });
 
 // Method to generate JWT
-userSchema.methods.generateAuthToken = function() {
+userSchema.methods.generateAuthToken = function () {
     const token = jwt.sign({
-        _id : this._id,
-        name : this.name,
-        email : this.email,
-        role : this.role,
-        image : this.image
-    }, process.env.JWT_SECRET, { expiresIn : '1h' });
+        _id: this._id,
+        name: this.name,
+        email: this.email,
+        role: this.role,
+        image: this.image,
+        phone: this.phone 
+    }, process.env.JWT_SECRET, { expiresIn: '1h' });
     return token;
 }
 
