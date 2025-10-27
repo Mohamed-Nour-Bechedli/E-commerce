f99c8ece918ea5d2647fd3c4219858ee4ec9c759
Mohamed Nour <mohamed-nour.bechdli@enis.tn>
2025-10-27 10:54:32 +0100

    added the get order by id route

--- server/routes/orderRouter.js
+++ server/routes/orderRouter.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
-const { createOrder, getAllOrders, getUserOrders, updateOrderStatus } = require('../controllers/orderController');
+const { createOrder, getAllOrders, getUserOrders, updateOrderStatus, getOrderById } = require('../controllers/orderController');
 const authRole = require('../middlewares/authRole');
 const authUser = require('../middlewares/authUser');
 
 // User routes
 router.post('/', authUser, createOrder);
 router.get('/', authUser, getUserOrders);
+router.get('/:id', authUser, getOrderById);
 
 // Admin routes
 router.get('/all', authUser, authRole, getAllOrders);
