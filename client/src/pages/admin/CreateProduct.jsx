1a7d0f1ec65e811aed0642a82c6d3aac3a1b8a72
Mohamed Nour <mohamed-nour.bechdli@enis.tn>
2025-10-31 10:27:00 +0100

    Created admin create product page

---
+++ client/src/pages/admin/CreateProduct.jsx
@@ -0,0 +1,10 @@
+const CreateProduct = () => {
+    return (
+        <div>
+            <h1 className="text-2xl font-bold mb-4">Create Product</h1>
+            <p>Here the admin can create new products.</p>
+        </div>
+    );
+};
+
+export default CreateProduct;
