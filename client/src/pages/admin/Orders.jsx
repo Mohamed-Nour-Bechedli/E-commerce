22365a20477fed7baeb4dec845019fd68d169d65
Mohamed Nour <mohamed-nour.bechdli@enis.tn>
2025-10-31 10:27:47 +0100

    Created the admin orders page

---
+++ client/src/pages/admin/Orders.jsx
@@ -0,0 +1,10 @@
+const AdminOrders = () => {
+    return (
+        <div>
+            <h1 className="text-2xl font-bold mb-4">Orders Management</h1>
+            <p>View and manage client orders here.</p>
+        </div>
+    );
+};
+
+export default AdminOrders;
