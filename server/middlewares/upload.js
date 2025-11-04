b68e389419a1192b6cdee16587ef74b7c2221260
Mohamed Nour <mohamed-nour.bechdli@enis.tn>
2025-11-04 14:50:24 +0100

    deleted a comment

--- server/middlewares/upload.js
+++ server/middlewares/upload.js
@@ -5,7 +5,7 @@ const cloudinary = require('../config/cloudinary');
 const storage = new CloudinaryStorage({
     cloudinary,
     params: {
-        folder: 'uploads', // folder name in Cloudinary
+        folder: 'uploads', 
         allowed_formats: ['jpg', 'png', 'jpeg', 'webp'],
         transformation: [{ width: 800, height: 800, crop: "limit" }]
     },
