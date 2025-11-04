4ab725743378909c4761cd3c57259369e949fc21
Mohamed Nour <mohamed-nour.bechdli@enis.tn>
2025-11-04 14:49:45 +0100

    Updated the upload middleware to use cloudinary to store and upload images

--- server/middlewares/upload.js
+++ server/middlewares/upload.js
@@ -1,30 +1,14 @@
 const multer = require('multer');
-const fs = require('fs');
-const path = require('path');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require('../config/cloudinary');
 
-const uploadDir = path.join(__dirname, '../uploads');
-
-// Ensure the uploads folder exists
-if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir, { recursive: true });
-}
-
-// Sanitize filenames (remove spaces & special chars)
-const sanitizeFilename = (name) => {
-    return name
-        .normalize('NFD')                  // split accents
-        .replace(/[\u0300-\u036f]/g, '')   // remove accents
-        .replace(/[^a-zA-Z0-9.]/g, '_');   // replace others with _
-};
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, uploadDir);
+const storage = new CloudinaryStorage({
+    cloudinary,
+    params: {
+        folder: 'uploads', // folder name in Cloudinary
+        allowed_formats: ['jpg', 'png', 'jpeg', 'webp'],
+        transformation: [{ width: 800, height: 800, crop: "limit" }]
     },
-    filename: (req, file, cb) => {
-        const safeName = sanitizeFilename(file.originalname);
-        cb(null, Date.now() + '-' + safeName);
-    }
 });
 
 const upload = multer({ storage });
